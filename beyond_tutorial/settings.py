c7d3406e82a078c7a9c364a7262efc561039e649
amitCohen2 <amitcohen435@gmail.com>
2023-03-01 11:28:50 +0200

    Data model classes and the migrations
    Edit settings.py, 0001_initial.py, 0002_test_data.py, models.py.

--- beyond_tutorial/settings.py
+++ beyond_tutorial/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'msgboard.apps.MsgboardConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
